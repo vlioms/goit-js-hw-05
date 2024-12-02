2918e7a53c949dc99fa8ee1c8e143480f2cc9fad
vlioms <villiomss14@gmail.com>
2024-12-02 22:11:48 +0100

    all

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,5 @@
 const sortByDescendingFriendCount = (users) =>
-    users.slice().sort((a, b) => b.friends.length - a.friends.length);
-
+    users.toSorted((a, b) => b.friends.length - a.friends.length);
 
 console.log(
     sortByDescendingFriendCount([
